54cd28b6e31e86c1b0778ad966b990d8a0962097
Matias Chomali <matias.chomali.dev@gmail.com>
2023-04-16 22:35:29 +0000

    turn middleware into class-based, start out leakingbucket

--- api.py
+++ api.py
@@ -3,11 +3,13 @@ import time
 from fastapi import FastAPI, Request
 from prometheus_fastapi_instrumentator import Instrumentator
 
-from rate_limiters import TokenBucket
+from rate_limiters import TokenBucketMiddleware, TokenBucketRateLimiter
 
 app = FastAPI()
+rate_limiter = TokenBucketRateLimiter(flow_rate=2, max_tokens=15)
+app.add_middleware(TokenBucketMiddleware, rate_limiter=rate_limiter)
+
 Instrumentator(excluded_handlers=["/metrics"]).instrument(app).expose(app)
-rate_limiter = TokenBucket(flow_rate=2, max_tokens=50)
 
 
 @app.get("/")
@@ -18,14 +20,14 @@ async def hello(name: str = None):
         return {"Hello World!"}
 
 
-@app.middleware("http")
-async def test_middleware(request: Request, call_next):
-    if request.url.path == "/metrics":
-        response = await call_next(request)
-        return response
+# @app.middleware("http")
+# async def test_middleware(request: Request, call_next):
+#     if request.url.path == "/metrics":
+#         response = await call_next(request)
+#         return response
 
-    if rate_limiter.receive_request(request):
-        response = await call_next(request)
-        return response
-    else:
-        print("Over limit")
+#     if rate_limiter.receive_request(request):
+#         response = await call_next(request)
+#         return response
+#     else:
+#         print("Over limit")
--- rate_limiters.py
+++ rate_limiters.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 from datetime import datetime as dt
+from typing import Callable, Dict
 
 from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
 
 
 class RateLimiter(ABC):
@@ -10,7 +12,20 @@ class RateLimiter(ABC):
         pass
 
 
-class TokenBucket(RateLimiter):
+class LeakingBucket(RateLimiter):
+    def __init__(self, max_size: int = 4) -> None:
+        self.buckets = {}
+
+    def receive_request(self, request: Request) -> bool:
+        ip = request.client.host
+        if ip not in self.buckets:
+            self.buckets[ip] = []
+        current_bucket = self.buckets[ip]
+        if len(current_bucket) > self.max_size:
+            return False
+
+
+class TokenBucketRateLimiter(RateLimiter):
     def __init__(self, max_tokens: int = 4, flow_rate: int = 2) -> None:
         self.max_tokens = max_tokens
         self.flow_rate = flow_rate
@@ -21,9 +36,10 @@ class TokenBucket(RateLimiter):
         current_time = dt.now()
         if ip not in self.buckets:
             self.buckets[ip] = {
-                "tokens": self.max_tokens,
+                "tokens": self.max_tokens - 1,
                 "last_request": current_time,
             }
+            return True
         current_bucket = self.buckets[ip]
         time_elapsed = (
             current_time - current_bucket["last_request"]
@@ -38,3 +54,21 @@ class TokenBucket(RateLimiter):
             return True
         else:
             return False
+
+
+class TokenBucketMiddleware(BaseHTTPMiddleware):
+    def __init__(
+        self, app: Callable, rate_limiter: TokenBucketRateLimiter
+    ) -> None:
+        super().__init__(app)
+        self.rate_limiter = rate_limiter
+
+    async def dispatch(self, request: Request, call_next):
+        if request.url.path == "/metrics":
+            response = await call_next(request)
+            return response
+        if self.rate_limiter.receive_request(request):
+            response = await call_next(request)
+            return response
+        else:
+            print("Over limit")
